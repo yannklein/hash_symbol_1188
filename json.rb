c02321a5b55f9da819e5d5e83c5a8a372de374dd
Yann Klein <yann.klein@me.com>
2021-04-10 11:06:33 +0900

    lecture done

--- json.rb
+++ json.rb
@@ -6,8 +6,6 @@ user = gets.chomp
 raw_json = URI.open("https://api.github.com/users/#{user}").read
 
 json_hash = JSON.parse(raw_json)
-# => { "name" => "Paris", "population" => 2211000 }
-
 p json_hash.class
 
 p "Hey! Your name is #{json_hash["name"]}"
